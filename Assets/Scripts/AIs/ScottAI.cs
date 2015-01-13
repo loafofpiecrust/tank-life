14b5a692432a08a027ba81dc882cf952a98c69f1
snwebster <coco2banutz@yahoo.com>
2015-01-13 16:12:10 -0600

    -Updating my AI script.

--- Assets/Scripts/AIs/ScottAI.cs
+++ Assets/Scripts/AIs/ScottAI.cs
@@ -1,146 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using Stuff;
 
-public class ScottAI : AI {
-
-
-	enum State
-	{
-		ST_FirstFire,
-		ST_Turn,
-		ST_Move,
-		ST_Brake,
-		ST_Stop
-	}
-
-	State currentState;
-	float currentStateTime = 0;
-	float lastTime = 0;
-
-	int shotsFired = 0;
-	
-	float stepTime = 1;
-
-	public override void StepLogic() {
-
-		currentStateTime+= Time.timeSinceLevelLoad - lastTime;
-		lastTime = Time.timeSinceLevelLoad;
+namespace ScottAI
+{
+	public class ScottAI : AI 
+	{					
 		
-		/*Transform enemy = GetNearestVisibleThing(playersLayer);
-		if (enemy != null) {
-			Debug.Log ("VISIBLE PLAYER!! at "+enemy.position);
-			TurnCannonTo (enemy.position);
-			player.FireCannon();
-		}*/
-
-		if(currentState == State.ST_FirstFire)
-		{
-			if(transform.position.x < 0)
-				TurnCannonTo (new Vector3(14.35922f, -13.00012f, 24.71606f));
+		private float nearestPlayerDist = 100.0f;
+		private Vector3 nearestPlayer = Vector3.zero;
+		
+		public override void StepLogic() {
+			
+			if (nearestPlayer != Vector3.zero) {
+				TurnCannonTo (nearestPlayer);
+				player.FireCannon ();
+				nearestPlayer = Vector3.zero;
+			}
 			else
-				TurnCannonTo (new Vector3(-14.44455f, 15.80382f, 24.81667f));
-			shotsFired++;
-			player.FireCannon();
-			if(shotsFired >5)
 			{
-				currentState = State.ST_Turn;
-				currentStateTime=0;
+				Vector3 forward = transform.localToWorldMatrix.MultiplyVector(Vector3.up);
+				Vector3 left = transform.localToWorldMatrix.MultiplyVector(Vector3.left);
+				Vector3 right = transform.localToWorldMatrix.MultiplyVector(Vector3.right);
+				if(IsBlocked(wallsLayer,forward,3.0f)
+				   || IsBlocked (wallsLayer,left,1.0f)
+				   || IsBlocked (wallsLayer,right,1.0f))
+				{
+					StopMoving();
+					if(rigidbody.velocity.magnitude <= 0.01f)
+					{
+						if(!IsBlocked (wallsLayer,left,5.0f))
+							Turn(-15);
+						else
+						   Turn(15);
+					}
+				}
+				else
+				{
+					MoveForward(300);
+				}
 			}
 		}
-		if(currentState == State.ST_Turn)
+		private void HandleSeeingAPlayer(Transform player)
 		{
-			Turn(45.0f);
-			stepTime = Random.Range(2,5);
-			MoveForward(stepTime);
-			currentState = State.ST_Move;
-			currentStateTime=0;
-		}
-		if(currentState == State.ST_Move)
-		{
-			if(currentStateTime >= stepTime)
+			//We only see ourselves
+			if(player == transform)
+				return;
+				
+			//nearestPlayer = player.transform.position;
+			Vector3 direction =  player.transform.position - transform.position;
+			if(!IsBlocked(wallsLayer, direction, direction.magnitude))
 			{
-				MoveForward(-1*stepTime);
-				currentState = State.ST_Brake;
-				currentStateTime = 0;
+				nearestPlayer = player.transform.position;
 			}
 		}
-		if(currentState == State.ST_Brake)
-		{
-			/*if(currentStateTime >= stepTime
-			   && enemy == null)
-			{
-				currentState = State.ST_Turn;
-				currentStateTime = 0;
-			}*/
+		public override void SeeObject(Transform other) {				
+			if (other.GetComponent<Player> ())
+				HandleSeeingAPlayer(other);
 		}
-		//MoveForward (2.0f);
-		//GameObject.fin
-		//TurnCannonTo (new Vector3(12.31893f,1.400977f,24.94851f));
-
-		
-
-		/*Transform nearestPlayer = GetNearestVisibleThing (playersLayer);
-
-		if(nearestPlayer != null)
-			HandlePlayerSeen()
-
-		//rigidbody.inertiaTensor = new Vector3(1, 1, 1);
-		
-		bool rightBlocked = IsBlocked (wallsLayer, transform.right);
-		
-		if (!rightBlocked && IsMoved ()) {
-			Turn (40);
-			MoveForward (2.0f);
-		}
-		
-		if (IsBlocked (wallsLayer, transform.up)) {
-			Debug.Log ("forward is blocked");
-			float turnAngle = 20;
-			StopMoving (0.25f);
-			if(!IsBlocked (wallsLayer, -transform.right)) {
-				Turn(-turnAngle);
-			}
-			else {
-				Turn(turnAngle);
-			}
-		} else if(IsMoved()) {
-			MoveForward (stepLength*2);
-		}
-		
-		if (!wallOnRight && !rightBlocked && IsMoved ()) {
-			StopMoving (0.2f);
-			Turn (15);
-		}
-		if (!wallOnRight && rightBlocked) {
-			wallOnRight = true;
-		} else if (wallOnRight && !rightBlocked) {
-			StopMoving (0.2f);
-			Turn (70);
-			wallOnRight = false;
-		}
-		
-		Transform p = GetNearestVisibleThing(playersLayer);
-		if (p) {
-			Debug.Log ("VISIBLE PLAYER!! at "+p.position);
-			TurnCannonTo (p.position);
-		}*/
-	}
-	void OnCollisionEnter(Collision col) {
-		if (IsBlocked (wallsLayer, transform.up)) {
-			Debug.Log ("WE HIT A WALL AHEAD");
-			float turnAngle = 20;
-			MoveBackwards (0.25f);
-			if(!IsBlocked (wallsLayer, -transform.right)) {
-				Turn(-turnAngle);
-			}
-			else {
-				Turn(turnAngle);
-			}
-		} else if (IsBlocked (wallsLayer, transform.right)) {
-			Turn (-5);
-		} else if (IsBlocked (wallsLayer, -transform.right)) {
-			Turn (5);
+		void OnCollisionEnter(Collision col) {
 		}
 	}
 }
